7287d6a93b25cbd7b76d5128954edaa687e9cf92
branz <janetteabelgas@yahoo.com.ph>
2012-07-08 13:07:33 +0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -44,8 +44,7 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
-        
-       
+            
       end
     end
   end
