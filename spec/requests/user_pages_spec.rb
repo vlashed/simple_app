0e017194485002660514238ef90c1fdcf6274c1e
branz <janetteabelgas@yahoo.com.ph>
2012-07-08 13:00:47 +0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,20 +4,21 @@ describe "User pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_selector('title', text: 'Sign up') }
   end
-  
-  describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
 
-  it { should have_selector('h1',    text: user.name) }
-  it { should have_selector('title', text: user.name) }
-  end
   describe "signup" do
 
     before { visit signup_path }
@@ -42,5 +43,5 @@ describe "User pages" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
     end
- end
+  end
 end
